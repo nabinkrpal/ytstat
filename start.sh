a4fddce8b4be50ae8e9f4190eaa07f796301491f
nabinkrpal <138757521+nabinkrpal@users.noreply.github.com>
2024-10-31 21:54:12 +0530

    added a

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Optionally, download spaCy model
+python -m spacy download en_core_web_sm
+
+# Start the Node.js server
+node server.js
