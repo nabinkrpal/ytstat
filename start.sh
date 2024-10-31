86ed60cb28f8933247c5a9ca5fd22a9aa4730903
nabinkrpal <138757521+nabinkrpal@users.noreply.github.com>
2024-11-01 01:29:51 +0530

    added a

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python -m venv env
-source .\env\Scripts\Activate.ps1
+# python -m venv env
+# source .\env\Scripts\Activate.ps1
 # # Install Python dependencies
 # pip install -r requirements.txt
 
@@ -24,5 +24,5 @@ python -m textblob.download_corpora || echo "TextBlob corpora download failed"
 python -m spacy download en_core_web_sm || echo "spaCy model download failed"
 
 # Start the Node.js server
-echo "Starting Node.js server..."
+echo "Starting Node.js server guys..."
 node server.js
